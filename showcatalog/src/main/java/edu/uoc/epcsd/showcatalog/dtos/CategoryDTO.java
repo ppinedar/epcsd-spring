5584e484742345826e5e3bf1aed3510d61421e77
ppinedar <72941559+ppinedar@users.noreply.github.com>
2022-02-15 09:48:24 +0100

    Add files via upload

---
+++ showcatalog/src/main/java/edu/uoc/epcsd/showcatalog/dtos/CategoryDTO.java
@@ -0,0 +1,14 @@
+package edu.uoc.epcsd.showcatalog.dtos;
+
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDTO {
+
+    private String name;
+
+}
