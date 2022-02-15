1304ff7a84feeaa0e6cdb0aafc71da583b02bd94
ppinedar <72941559+ppinedar@users.noreply.github.com>
2022-02-15 09:57:52 +0100

    Delete showcatalog directory

--- showcatalog/src/main/java/edu/uoc/epcsd/showcatalog/repositories/ShowRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.uoc.epcsd.showcatalog.repositories;
-
-import edu.uoc.epcsd.showcatalog.entities.Show;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShowRepository extends JpaRepository<Show, Long> {
-
-}
